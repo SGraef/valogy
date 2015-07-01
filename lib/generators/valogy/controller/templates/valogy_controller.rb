c88e09eeddac75f0530c1073bf9cf717eb168b79
Sascha Graef <sgraef@informatik.uni-bremen.de>
2015-07-01 16:06:04 +0200

    generator with filled UNTESTED content

--- lib/generators/valogy/controller/templates/valogy_controller.rb
+++ lib/generators/valogy/controller/templates/valogy_controller.rb
@@ -5,6 +5,12 @@ class ValogyController < ApplicationController
   end
 
   def create
-
+		uploaded_io = params[:ontology]
+		if Valogy::Parser.parse(Rails.root.join('public', 'uploads', uploaded_io.original_filename))
+			redirect_to root_path, notice: 'Validations successfully updated'
+		else
+			render action: 'new'
+		end
   end
+
 end
