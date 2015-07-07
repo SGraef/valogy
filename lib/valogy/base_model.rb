cd0b385ef83f2280b2eec96316471c21c4e66e02
Sascha Graef <sgraef@informatik.uni-bremen.de>
2015-07-07 11:59:10 +0200

    move the rescue to the correct place to get all changes

--- lib/valogy/base_model.rb
+++ lib/valogy/base_model.rb
@@ -6,18 +6,34 @@ module Valogy
 
     def set_consistency
       self.consistent = true
-      self.save
-    rescue ActiveRecord::StatementInvalid => e
-      validation = e.message.match('check\ constraint\ \\"([\S]*)\\"')[1]
-      if validation.include?("valogy")
-				error_string = I18n.t("valogy.#{validation}")
-				self.errors[I18n.t("valogy.model.#{validation}").to_sym] << error_string
-				raise(ActiveRecord::RecordInvalid.new(self))
-			else
-				raise(e)
-			end
+      self.save!
     end
 
+    def save(*)
+      super(self)
+      rescue ActiveRecord::StatementInvalid => e
+        validation = e.message.match('check\ constraint\ \\"([\S]*)\\"')[1]
+  			if validation.include?("valogy")
+  				error_string = I18n.t("valogy.#{validation}")
+  				self.errors[I18n.t("valogy.model.#{validation}").to_sym] << error_string
+          return false
+        else
+    			raise(e)
+        end
+      end
+
+    def save!(*)
+      super(self)
+      rescue ActiveRecord::StatementInvalid => e
+        validation = e.message.match('check\ constraint\ \\"([\S]*)\\"')[1]
+  			if validation.include?("valogy")
+  				error_string = I18n.t("valogy.#{validation}")
+  				self.errors[I18n.t("valogy.model.#{validation}").to_sym] << error_string
+  				raise(ActiveRecord::RecordInvalid.new(self))
+  			else
+  				raise(e)
+  			end
+      end
 
     def consistent?
       self.consistent
