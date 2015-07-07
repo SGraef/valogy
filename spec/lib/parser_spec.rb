c3abf790a2d73c144ad9f0399f27dbc8bf10d334
Sascha Graef <sgraef@informatik.uni-bremen.de>
2015-07-07 11:56:12 +0200

    adding more checks to parser test

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -6,7 +6,9 @@ context "constraints which is for attributes of the model" do
     context "constraint generation" do
       it "raise no error" do
         expect(Valogy::BaseModel.connection.execute("SELECT * FROM pg_constraint WHERE conname='valogy_users_username_not_null' ").ntuples).to eq(0)
+        expect(Valogy::BaseModel.connection.execute("SELECT * FROM pg_constraint WHERE conname='valogy_slots_sheet_id_minmal' ").ntuples).to eq(0)
         expect{Valogy::Parser.parse("#{fixture_path}/test.owl")}.not_to raise_error
+        expect(Valogy::BaseModel.connection.execute("SELECT * FROM pg_constraint WHERE conname='valogy_slots_sheet_id_minimal' ").ntuples).to eq(1)
         expect(Valogy::BaseModel.connection.execute("SELECT * FROM pg_constraint WHERE conname='valogy_users_username_not_null' ").ntuples).to eq(1)
       end
     end
