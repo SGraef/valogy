6468fac3d304a8f98c9ed65960d2460fc4382435
Sascha Graef <sgraef@informatik.uni-bremen.de>
2015-07-07 15:42:36 +0200

    generator are now generate functional code

--- lib/generators/valogy/controller/templates/valogy_controller.rb
+++ lib/generators/valogy/controller/templates/valogy_controller.rb
@@ -6,7 +6,7 @@ class ValogyController < ApplicationController
 
   def create
 		uploaded_io = params[:ontology]
-		if Valogy::Parser.parse(Rails.root.join('public', 'uploads', uploaded_io.original_filename))
+		if Valogy::Parser.parse(uploaded_io.path)
 			redirect_to root_path, notice: 'Validations successfully updated'
 		else
 			render action: 'new'
