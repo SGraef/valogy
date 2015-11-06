d75977186f6fa7079305cd152addd4a6e3c8401f
Sascha Graef <sgraef@informatik.uni-bremen.de>
2015-11-06 12:46:47 +0100

    adding missing attr_accessor

--- lib/valogy/restrictions/maximal_restriction.rb
+++ lib/valogy/restrictions/maximal_restriction.rb
@@ -1,6 +1,8 @@
 module Valogy
   class Maximal_Restriction < Valogy::Restriction
 
+    attr_accessor :foreign_table
+
     def initialize
       self.constraint_suffix = "maximal"
       self.operator = "<="
