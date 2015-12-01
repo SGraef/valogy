1597e515b0a215e9ffa98e4d885760f6a9bba752
Sascha Graef <sgraef@informatik.uni-bremen.de>
2015-12-01 10:27:29 +0100

    remove code duplication

--- lib/valogy/restrictions/existence_restriction.rb
+++ lib/valogy/restrictions/existence_restriction.rb
@@ -2,27 +2,13 @@ module Valogy
   class ExistenceRestriction < Valogy::Restriction
 
     def resolve
-      if property.class == DataProperty
-        existence
-      elsif property.class == ObjectProperty
-        existence_between_models
-      end
-    end
-
-    def initialize
-      self.constraint_suffix = "not_null"
-    end
-
-    def existence
       self.constraint_name = generate_constraint_name(entity.corresponding_model.table_name)
     entity.corresponding_model.connection.execute("ALTER TABLE #{entity.corresponding_model.table_name} ADD CONSTRAINT
     #{self.constraint_name} CHECK (#{column} IS NOT NULL)")
     end
 
-    def existence_between_models
-        self.constraint_name = generate_constraint_name(entity.corresponding_model.table_name)
-        entity.corresponding_model.connection.execute("ALTER TABLE #{entity.corresponding_model.table_name} ADD CONSTRAINT
-        #{self.constraint_name} CHECK (#{column} IS NOT NULL)")
+    def initialize
+      self.constraint_suffix = "not_null"
     end
 
   end
